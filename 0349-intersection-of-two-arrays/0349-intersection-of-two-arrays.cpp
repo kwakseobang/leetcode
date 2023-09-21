8c3b83a5033c4336b570b569dcb35e95237f04ce
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-09-21 21:07:24 +0900

    Time: 3 ms (85.20%), Space: 10.4 MB (41.98%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>result;
+        //일부러 set 안쓰고 해봤슴
         for(int i = 0; i<nums2.size();i++){
             if(count(nums1.begin(),nums1.end(),nums2[i]) && !(count(result.begin(),result.end(),nums2[i]))){
                 result.push_back(nums2[i]);
