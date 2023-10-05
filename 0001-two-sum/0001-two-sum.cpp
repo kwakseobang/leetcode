614b30d0c7a9fe3766b8e2a834d89007c690c0ca
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-05 12:53:46 +0900

    Time: 401 ms (13.56%), Space: 10.6 MB (51.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,7 +8,7 @@ public:
                 if(nums[i] + nums[j] == target){
                     result.push_back(i);
                     result.push_back(j);
-                    return result;
+                    
                 }
 
             }
