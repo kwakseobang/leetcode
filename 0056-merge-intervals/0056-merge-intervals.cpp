faeba9816707cccc40f86e256d40ed0a3c5e2b0b
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-18 18:33:56 +0900

    Time: 33 ms (40.09%), Space: 19.4 MB (53.58%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,15 +4,17 @@ public:
         vector<vector<int>> res;
         sort(intervals.begin(),intervals.end()); // 정렬
         int j = 0;
+        int i = 1;
         res.push_back(intervals[0]);
-        for(int i = 1; i<intervals.size();i++){
-            if (res[j][1]>=intervals[i][0]){
+        while(i<intervals.size()){
+            if (res[j][1] >= intervals[i][0]){
                 res[j][1] = max(res[j][1],intervals[i][1]);
             }
             else{
                 j++;
                 res.push_back(intervals[i]);
             }
+            i++;
             }
             
         return res;
