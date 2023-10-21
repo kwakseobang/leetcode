8c6fa7ae5995b9d39a2aff50a6e1d27377c5c86c
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-21 21:00:40 +0900

    Time: 6 ms (92.26%), Space: 11.8 MB (11.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        map<int,int> map1;
-        int len = nums.size();
-        for(int i = 0; i<len;i++){
-            if(map1.find(target-nums[i]) != map1.end()) {//값 찾은 경우
+        vector<int>res;
+        map<int,int>m;
+        for(int i = 0; i<nums.size();i++){
+            int key = target-nums[i];
+            if(m.find(key) != m.end()){
+                res.push_back(m[key]);
                 res.push_back(i);
-                res.push_back(map1[target-nums[i]]);
                 break;
             }
-            map1[nums[i]] = i;
-    }
+            m[nums[i]] = i;
+        }
         return res;
     }
 };
