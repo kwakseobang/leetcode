10baefd7f79f514a1b78f5a26de68190fa251d8a
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-26 02:56:58 +0900

    Time: 85 ms (40.21%), Space: 14.7 MB (68.30%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
       int coinChange(vector<int>& coins, int amount) {
-        vector<int>table(amount+1,INT_MAX-1); //각 테이블 인덱스는 인덱스 가격의 최소값
-        table[0] = 0;
-        int len = coins.size();
-        for(int i = 0; i<len;i++){
-            for(int j = 1; j < amount+1; j++){
-                if(j-coins[i]>=0){
-                table[j] = min(table[j],table[j-coins[i]]+1);
-                }
-            }
-        }
-        return (table[amount] != INT_MAX-1) ? table[amount]: -1;
+          vector<int>dp(amount+1,INT_MAX-1);
+          dp[0] = 0;
+          for(int i = 0; i<coins.size();i++){
+              for(int j = 1; j<=amount; j ++){
+                  if(j-coins[i]>=0){
+                      dp[j] = min(dp[j],dp[j-coins[i]]+1);
+                  }
+              }
+          }
+          return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
+          
     }
 
     
