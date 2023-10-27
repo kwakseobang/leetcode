ceef6794ee9e6b0b26aa5b7791a34c5f122a0664
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-27 12:32:48 +0900

    Time: 14 ms (22.29%), Space: 7.7 MB (52.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string s, string t){
         if(s.size() != t.size()) return false;
-    map<char,int>m1;
-    map<char,int>m2;
-    for(int i = 0; i<s.size(); i++){
-        if(!m1.contains(s[i])){
-            m1[s[i]] = 1;
+        map<char,int>m1;
+        int len = s.size();
+        for(int i = 0;i<len;i++){
+            m1[s[i]]++;
+            m1[t[i]]--;
         }
-        else{
-            int tmp = m1[s[i]];
-            m1[s[i]] = tmp + 1;
-        }
-        if(!m2.contains(t[i])){
-            m2[t[i]] = 1;
-        }
-        else{
-            int tmp = m2[t[i]];
-            m2[t[i]] = tmp + 1;
-        }
-    }
-    // for(int i = 0; i<t.size(); i++){
-    //     if(!m2.contains(t[i])){
-    //         m2[t[i]] = 1;
-    //     }
-    //     else{
-    //         int tmp = m2[t[i]];
-    //         m2[t[i]] = tmp + 1;
-    //     }
-    // }
-    for(auto &pair: m1){
-        char key = pair.first;
-        if(m1[key] != m2[key]){
-            return false;
-        }
+       for(auto pair:m1){
+           if(pair.second) return false;
+       }
+     return true;
 
     }
-    return true;
-    }
+    
 };
