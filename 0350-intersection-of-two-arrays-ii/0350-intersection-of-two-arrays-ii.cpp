17cb516ce1ec4311fdf0a6da6d68d7009f0eda2f
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-29 19:06:21 +0900

    Time: 11 ms (10.55%), Space: 10.5 MB (46.88%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int>res;
-        int idx = 0;
         while(!nums1.empty() && !nums2.empty()){
-            if(find(nums1.begin(),nums1.end(),nums2[idx]) != nums1.end() ){
-                int rmIdx = find(nums1.begin(),nums1.end(),nums2[idx]) - nums1.begin();
-                res.push_back(nums2[idx]);
+            if(find(nums1.begin(),nums1.end(),nums2[0]) != nums1.end() ){
+                int rmIdx = find(nums1.begin(),nums1.end(),nums2[0]) - nums1.begin();
+                res.push_back(nums2[0]);
                 nums1.erase(nums1.begin() + rmIdx);
             }
                 nums2.erase(nums2.begin());
-
-    
         }
         return res;
     }
