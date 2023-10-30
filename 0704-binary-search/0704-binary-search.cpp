ee2a948783ded90f0b039dd55ac578c7bf11794f
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-10-30 15:50:15 +0900

    Time: 28 ms (61.00%), Space: 28 MB (17.91%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int len = nums.size();
-        if(len == 1){
-          return (nums[0] == target) ?  0 : -1;
-        }
+        if(len == 1) return (nums[0] == target) ?  0 : -1; //배열 개수가 1개일 때
         for(int i = (len/2)-1; i>=0; i--){
             int left = (i*2) + 1;
             int right = (i*2) + 2;
             if(nums[i] == target) return i;
-            
-            else if(nums[left] == target){
-                return left;
-            }
-            else if(right >= len){
-                continue;
-            }
-            else if(nums[right] == target){
-                return right;
-            }
+            else if(nums[left] == target)return left;
+            else if(right >= len) continue;
+            else if(nums[right] == target) return right;
             
         }
         return -1;
