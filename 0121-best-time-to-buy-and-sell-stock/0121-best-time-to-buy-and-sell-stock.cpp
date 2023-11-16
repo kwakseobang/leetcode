68072d2f558f98102a623eb510a16bfba9609ccd
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-11-16 16:41:26 +0900

    Time: 84 ms (92.26%), Space: 93.9 MB (7.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,10 +6,10 @@ public:
         int cmp = INT_MAX;
         //n번째 날에 어떤 걸 팔아야 극대화가 되냐 라는 느낌으로 풀기.
         for(int i = 1; i < len; i++){
-            cmp = min(cmp,prices[i-1]);
+            cmp = cmp < prices[i-1] ? cmp:prices[i-1];
+            // cmp = min(cmp,prices[i-1]);
             maxProfit =  (prices[i]- cmp) > maxProfit ? (prices[i]- cmp):maxProfit;
         }
         return maxProfit;
-        
     }
 };
