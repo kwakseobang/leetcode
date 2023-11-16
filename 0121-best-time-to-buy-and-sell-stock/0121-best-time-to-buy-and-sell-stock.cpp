22b3091673ee2759de8e4ecd983452178dd28430
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-11-16 16:40:32 +0900

    Time: 98 ms (48.60%), Space: 93.8 MB (27.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int len = prices.size();
-        vector<int> dp(len+1,0);
-        dp[0] = 0;
         int maxProfit = 0;
-        int cmp = 99999999999999;
+        int cmp = INT_MAX;
         //n번째 날에 어떤 걸 팔아야 극대화가 되냐 라는 느낌으로 풀기.
         for(int i = 1; i < len; i++){
             cmp = min(cmp,prices[i-1]);
-             dp[i] = max(prices[i]-cmp,dp[i-1]);
+            maxProfit =  (prices[i]- cmp) > maxProfit ? (prices[i]- cmp):maxProfit;
         }
-        return *max_element(dp.begin(),dp.end());
+        return maxProfit;
         
     }
 };
