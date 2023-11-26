9734f6975869e56bdba79ed87410cf59c141e3ec
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2023-11-26 12:04:23 +0900

    Time: 3 ms (41.52%), Space: 6.9 MB (17.84%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        map<char,string> m; 
+       map<char,string> m; 
         map<string,char> m2;
         stringstream ss(s);
         vector<string> v;
         string str;
-        while(ss >> str){
+        while (getline(ss, str, ' ')){
             v.push_back(str);
         }
         if(v.size() != pattern.size()) return false;
         for(int i=0; i<pattern.size();i++){
-            if(m.find(pattern[i]) == m.end()){
+            if(m.find(pattern[i])!= m.end()){ //key가 존재하는경우 
+                if (m2[v[i]] != pattern[i]) return false;
+            }
+            else {
+                if(m2.contains(v[i]))return false;
                 m2[v[i]] = pattern[i]; //키를 문자로 값을 패턴으로
                 m[pattern[i]] = v[i]; //반대
-        }
-        }
-        for(int i = 0; i< pattern.size(); i++){
-            if (m2[v[i]] != pattern[i]) return false;
+            }
         }
         return true;
     }
