18402d8ccfb8bfa38543619e78aede87d78b69c1
kwakseobang <140421374+kwakseobang@users.noreply.github.com>
2024-01-01 23:47:25 +0900

    Time: 22 ms (49.83%), Space: 11.7 MB (13.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> res;
         for(int i = 0; i<nums.size(); i++){
             int cmp=target - nums[i];
-            if(m.contains(cmp)){
+            if(m.count(cmp) != 0){
                 res.push_back(m[cmp]);
                 res.push_back(i);
                 break;
